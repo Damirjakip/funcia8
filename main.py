b2db0355ece95772a5c2b4996d435b8f4f9c6f7a
damir <damirjakip34@gmail.com>
2023-04-13 15:54:37 +0600

    Damir

---
+++ main.py
@@ -0,0 +1,17 @@
+def compose_functions(f, g):
+    """Возвращает новую функцию h(x), которая является композицией функций f и g"""
+    def h(x):
+        """Композиция функций f и g"""
+        return f(g(x))
+    return h
+
+def f(x):
+    """Возвращает квадрат числа x"""
+    return x**2
+
+def g(x):
+    """Возвращает число x плюс 1"""
+    return x + 1
+
+h = compose_functions(f, g)
+print(h(2)) 
